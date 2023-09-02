59922bfdf296a9f0f2ec63374ec84bc3fe37af8a
Evelise Karen Guenda <118639499+evydsg@users.noreply.github.com>
2023-09-01 20:37:54 -0500

    Question2

---
+++ MaxConsecutives.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max =0;
+        
+        for(int a = 0; a < nums.length; a++)
+        {
+                if(nums[a]==1)
+                {
+                    count++;
+        
+                }
+                else
+                {
+                    count =0;
+                 
+                }
+            max = Math.max(count, max);
+                
+        }
+        return max;
+    }
+}
